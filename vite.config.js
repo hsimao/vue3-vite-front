501d541d14ace9d263921018713309333bc169aa
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-07-18 15:31:01 +0800

    feat: vite setting alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { join } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    // 軟連結
+    alias: {
+      // @/components/about.vue => /src/components/about.vue
+      '@': join(__dirname, '/src')
+    }
+  }
 })
