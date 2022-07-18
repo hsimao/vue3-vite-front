776a057576aa6d89330a5507052f6aa3caa203b2
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-07-18 23:12:30 +0800

    feat: mobile navigation style 70%

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,9 +1,16 @@
 <template>
-  <ul
-    class="sticky top-0 left-0 z-10 flex overflow-hidden overflow-x-auto bg-white p-1"
-  >
+  <div class="sticky top-0 left-0 z-10 bg-white"></div>
+  <ul class="relative flex overflow-hidden overflow-x-auto p-1">
+    <!-- 漢堡按鈕 -->
     <li
-      class="shrink-0 px-1.5 py-0.5 text-xs text-zinc-600 duration-200"
+      class="fixed top-0 right-[-1px] z-20 flex h-4 items-center bg-white px-1 shadow-l-white"
+    >
+      <m-svg-icon name="hamburger" class="h-1.5 w-1.5" />
+    </li>
+
+    <!-- 選單 -->
+    <li
+      class="shrink-0 px-1.5 py-0.5 text-xs text-zinc-600 duration-200 last:mr-4"
       v-for="category in categorys"
       :key="category.id"
     >
