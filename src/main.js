328948a211443cb96be3c530fe4c6b4b0485e2fa
Hsi Mao CHEN <hsimaochen@Hsis-MacBook-Pro.local>
2022-07-18 22:22:30 +0800

    feat: initRem util

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import './styles/index.scss'
 import App from './App.vue'
 import router from './router'
+import { initRem } from '@/utils/flexible'
+
+initRem()
 
 createApp(App).use(router).mount('#app')
--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -5,3 +5,27 @@ const { width } = useWindowSize()
 export function isMobile(pcWidth = PC_DIVICE_WIDTH) {
   return width.value < pcWidth
 }
+
+/*
+ * 動態修改 rem 基準值, 最大 40px
+ * 依據裝置寬度進行換算, 把計算出來的值放進 html 的 font-size
+ */
+export const initRem = (customSize) => {
+  const MAX_FONT_SIZE = 40
+
+  // 監聽 html document 解析完成的事件
+  document.addEventListener('DOMContentLoaded', () => {
+    const html = document.querySelector('html')
+
+    // 如果有接收到 customSize 參數, 直接套用不另外計算
+    if (customSize && typeof customSize === 'number') {
+      html.style.fontSize = customSize
+      return
+    }
+
+    // 依據裝置寬度計算 fontSize, 最大不得超過 MAX_FONT_SIZE
+    let fontSize = window.innerWidth / 10
+    fontSize = fontSize > MAX_FONT_SIZE ? MAX_FONT_SIZE : fontSize
+    html.style.fontSize = `${fontSize}px`
+  })
+}
