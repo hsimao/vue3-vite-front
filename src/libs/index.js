312074821eeb9116b4fb2e3c732ff148d978c7eb
hsimao <e087754958@gmail.com>
2022-07-20 20:27:21 +0800

    feat: 透過 vite glob, vue defineAsyncComponent, 實現自動註冊全局組件邏輯

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,9 +1,17 @@
-import svgIcon from './svg-icon/index.vue'
-import popup from './popup/index.vue'
+import { defineAsyncComponent } from 'vue'
 
 export default {
   install(app) {
-    app.component('m-svg-icon', svgIcon)
-    app.component('m-popup', popup)
+    // 獲取 libs 底下所有 .vue 檔案
+    const components = import.meta.glob('./*/index.vue')
+
+    // 將 components obj 轉成陣列遍歷
+    for (const [fullPath, fn] of Object.entries(components)) {
+      // 拿路徑來組成要註冊的組件名稱 './popup/index.vue' => 'm-popup'
+      const componentName = 'm-' + fullPath.replace('./', '').split('/')[0]
+
+      // 註冊組件
+      app.component(componentName, defineAsyncComponent(fn))
+    }
   }
 }
